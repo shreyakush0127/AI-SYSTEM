104c5db4127d69ad6213c380f5aecb6f32ca4477
shreyakush0127 <shreyakush06@gmail.com>
2025-09-08 00:03:29 +0530

    Authentication

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { reactRouter } from "@react-router/dev/vite";
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import animate from 'tailwindcss-animate'
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
